e59b041b5ed880e9189a467bbce83135551fca8d
Raf Van Baelen <rafvanbaelen@gmail.com>
2015-06-04 13:29:41 +0200

    Initial commit of Recruiting solution. Implementing create screening scenario.

---
+++ Recruiting.Domain/Recruiting.Domain/Screening.cs
@@ -0,0 +1,40 @@
+﻿namespace Recruiting.Domain
+{
+    using System;
+
+    internal class Screening
+    {
+        private readonly DateTime date;
+        private readonly string candidate;
+
+        private Screening(DateTime date, string candidate)
+        {
+            this.date = date;
+            this.candidate = candidate;
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+        }
+
+        public string Candidate
+        {
+            get
+            {
+                return candidate;
+            }
+        }
+
+        internal class ScreeningFactory
+        {
+            public Screening Create(DateTime date, string candidate)
+            {
+                return new Screening(date, candidate);
+            }
+        }
+    }
+}
---
+++ Recruiting.Domain/Recruiting.ScenarioTests/CreateScreeningTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recruiting.Domain;
+
+namespace Recruiting.ScenarioTests
+{
+    [TestClass]
+    public class CreateScreeningTests
+    {
+        [TestMethod]
+        public void CreateScreening_x_ScreeningPersisted()
+        {
+            Screening.ScreeningFactory screeningFactory = new Screening.ScreeningFactory();
+
+            var screening = screeningFactory.Create(new DateTime(2015, 02, 24), "Luc Leysen");
+
+            Assert.IsNotNull(screening);
+        }
+    }
+}
