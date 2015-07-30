6298142aefab4b80b8f711f4dc6696349a49bc72
Raf Van Baelen <rafvanbaelen@gmail.com>
2015-07-30 13:02:05 +0200

    Added angular ui (tried in a seperate web app, but got CORS(?) issues)

---
+++ Recruiting.Domain/Recruiting.Web.Angular/app.js
@@ -0,0 +1,16 @@
+﻿angular.module('recruitingApp', [])
+    .controller('screeningsController', [
+        '$scope', '$http', function ($scope, $http) {
+            $scope.screening = {};
+            $scope.id = 0;
+
+            $scope.createScreening = function () {
+                $http.post('http://localhost:64677/api/screenings/', $scope.screening)
+                    .then(function (response) {
+                        var id = response.data;
+
+                        $scope.id = id;
+                    });
+            };
+        }
+    ]);
---
+++ Recruiting.Domain/Recruiting.Web/app.js
@@ -0,0 +1,40 @@
+﻿angular.module('recruitingApp', ['ngRoute'])
+    .config(['$routeProvider',
+      function ($routeProvider) {
+          $routeProvider.
+            when('/screenings', {
+                templateUrl: 'partials/screenings.html',
+                controller: 'screeningsController'
+            }).
+            when('/screenings/:id', {
+                templateUrl: 'partials/screening.html',
+                controller: 'screeningController'
+            }).
+            otherwise({
+                redirectTo: '/screenings'
+            });
+      }])
+    .controller('screeningsController', [
+        '$scope', '$http', '$location', function ($scope, $http, $location) {
+            $scope.screening = {};
+
+            $scope.createScreening = function () {
+                $http.post('http://localhost:64677/api/screenings/', $scope.screening)
+                    .then(function (response) {
+                        var data = response.data;
+
+                        $location.path('/screenings/' + data.Id);
+                    });
+            };
+        }
+    ])
+    .controller('screeningController', [
+        '$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+            $scope.screening = {};
+
+            $http.get('http://localhost:64677/api/screenings/' + $routeParams.id)
+                .then(function (response) {
+                    $scope.screening = response.data;
+                });
+        }
+    ]);
