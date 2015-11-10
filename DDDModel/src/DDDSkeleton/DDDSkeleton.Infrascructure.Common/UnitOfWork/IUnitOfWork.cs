1527b032934f0c8d91ab63e4e3b83bc79bad05bc
luc.leysen@cegeka.be <lleybe&1956>
2015-11-10 09:28:55 +0100

    readded IUnitofWork

---
+++ DDDModel/src/DDDSkeleton/DDDSkeleton.Infrascructure.Common/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using DDDSkeleton.Infrascructure.Common.Domain;
+
+namespace DDDSkeleton.Infrascructure.Common.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        void RegisterUpdate(IAggregateRoot aggregateRoot, IUnitOfWorkRepository repository);
+        void RegisterInsertion(IAggregateRoot aggregateRoot, IUnitOfWorkRepository repository);
+        void RegisterDeletion(IAggregateRoot aggregateRoot, IUnitOfWorkRepository repository);
+        void Commit();
+    }
+}
